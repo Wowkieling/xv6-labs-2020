1510410859424c1ae724c2f00bf81c8673fe28a8
Wokieling <lihlsophie@163.com>
2024-06-17 00:26:34 +0800

    start syscall trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -291,6 +291,8 @@ fork(void)
 
   safestrcpy(np->name, p->name, sizeof(p->name));
 
+  np->trace_mask = p->trace_mask; //将trace_mask拷贝到子进程
+
   pid = np->pid;
 
   np->state = RUNNABLE;
--- kernel/proc.h
+++ kernel/proc.h
@@ -103,4 +103,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int trace_mask; // trace(int) 调用参数 1 << syscall_id
 };
