2b94e2d72cca45f50b49106d0f03554fa351df0d
Wokieling <lihlsophie@163.com>
2025-10-08 21:48:58 +0800

    feat: a kernel page table per process

--- kernel/defs.h
+++ kernel/defs.h
@@ -159,9 +159,11 @@ int             uartgetc(void);
 
 // vm.c
 void            kvminit(void);
+pagetable_t     kvminit_newpgtbl();
+void            kvmfreepgtbl(pagetable_t);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+uint64          kvmpa(pagetable_t, uint64);
+void            kvmmap(pagetable_t, uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
